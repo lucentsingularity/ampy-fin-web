f0a071becff425470e4763e8c86f448a0311a8a9
lucentsingularity <53275042+lucentsingularity@users.noreply.github.com>
2025-02-14 07:29:43 -0800

    Fix: #2
    Hardcoded the timeframes in the API Route

--- src/app/api/alpaca/latest_ticks/[symbol]/route.js
+++ src/app/api/alpaca/latest_ticks/[symbol]/route.js
@@ -5,12 +5,16 @@ export async function GET(request, { params }) {
   try {
     const symbol = (await params).symbol
     const searchParams = request.nextUrl.searchParams
-    const timeframe = searchParams.get('timeframe') || '2Min' // Available: 2Min, 15Min, 1Hour, 3Hour, 1Day
-    const limit = searchParams.get('limit') || '720'
-    const currentTime = new Date();
-    const startTime = new Date(currentTime.getTime() - (limit * 2 * 60 * 1000));
-    const startTimeISO = startTime.toISOString();
-    const req = await fetch(`https://data.alpaca.markets/v2/stocks/bars?start=${startTimeISO}&symbols=${symbol}&timeframe=${timeframe}&adjustment=raw&feed=sip&sort=desc`, {
+    const timeframe = searchParams.get('timeframe') || '2min' // Available: 2Min, 15Min, 1Hour, 3Hour, 1Day
+    const timeframes = {
+      '2min': { frame: 'Day', startTime: new Date(new Date().getTime() - (2 * 60 * 1000 * 720)) },
+      '15min': { frame: 'Week', startTime: new Date(new Date().getTime() - (15 * 60 * 1000 * 672)) },
+      '1hour': { frame: 'Month', startTime: new Date(new Date().getTime() - (60 * 60 * 1000 * 720)) },
+      '3hour': { frame: '3Month', startTime: new Date(new Date().getTime() - (3 * 60 * 60 * 1000 * 720)) },
+      '1day': { frame: 'Year', startTime: new Date(new Date().getTime() - (24 * 60 * 60 * 1000 * 365)) },
+    }
+    const startTimeISO = timeframes[timeframe].startTime.toISOString();
+    const req = await fetch(`https://data.alpaca.markets/v2/stocks/bars?start=${startTimeISO}&symbols=${symbol}&timeframe=${timeframe}&limit=1000&adjustment=raw&feed=sip&sort=desc`, {
       headers: {
         'APCA-API-KEY-ID': process.env.APCA_API_KEY_ID,
         'APCA-API-SECRET-KEY': process.env.APCA_API_KEY_SECRET,
